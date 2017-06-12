6c4a40bdb5760d0393a2da0cc6f7e7138112b6be
Christophe Braud <chbperso@gmail.com>
2017-06-12 19:50:59 +0200

    - First commit

---
+++ Sources/MyClass.swift
@@ -0,0 +1,13 @@
+//
+//  MyClass.swift
+//  Template
+//
+//  Created by me on 27/05/2017.
+//
+//
+
+import Foundation
+
+public class MyClass {
+  
+}
---
+++ Sources/Template.h
@@ -0,0 +1,19 @@
+//
+//  Template.h
+//  Template
+//
+//  Created by me on 26/05/2017.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Template.
+FOUNDATION_EXPORT double Template_VersionNumber;
+
+//! Project version string for Template.
+FOUNDATION_EXPORT const unsigned char Template_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Template_/PublicHeader.h>
+
+
