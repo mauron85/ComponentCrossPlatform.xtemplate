948f0442cf3f6a15acfd7bc9078df1412839d593
Christophe Braud <chbperso@gmail.com>
2017-08-14 14:21:25 +0200

    - Add Unit Tests and AppTests for Unit Tests - Update .gitignore

---
+++ AppTest macOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AppTest macOS
+//
+//  Created by me on 14/08/2017.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
