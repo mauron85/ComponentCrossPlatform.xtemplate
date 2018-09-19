676c03dcce38250afc424091d552ec8b798df15d
TofPlay <chbperso@gmail.com>
2018-09-19 13:54:09 +0200

    Update for Xcode 10 Swift 4 without UnitTests

--- Package.swift
+++ Package.swift
@@ -20,10 +20,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Template",
-            dependencies: []),
-        .testTarget(
-            name: "UnitTests",
-            dependencies: ["Template"],
-            path: "UnitTests")
+            dependencies: [])
     ]
 )
